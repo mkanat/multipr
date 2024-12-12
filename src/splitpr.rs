51a55b1c3f4d6ab5c55e39f65d5d5c46ccad3682
Max Kanat-Alexander <avatraxiom@gmail.com>
2024-12-12 08:56:56 -0500

    Replace the patch crate with our own patch splitter that does what we actually need.

--- src/splitpr.rs
+++ src/splitpr.rs
@@ -1,11 +1,81 @@
 use std::error::Error;
 use std::io;
 
-use patch::Patch;
-
 fn main() -> Result<(), Box<dyn Error>> {
     let input = io::read_to_string(io::stdin())?;
-    let patch = Patch::from_multiple(&input).map_err(|e| e.to_string())?;
-    println!("{:#?}", patch);
+    let patch_files = split_diff(input);
+    println!("{:#?}", patch_files);
     Ok(())
+}
+
+#[derive(Debug)]
+struct PatchFile {
+    old: String,
+    new: String,
+    contents: String,
+}
+
+
+/*
+Originally I was using the patch crate to parse patches, but it does both
+less and more than I need. I don't need to understand hunks (which patch
+does) but I do want to, ideally, preserve all the header content of each
+patch (which patch does not).
+
+This does not borrow the input string, because in all callers we never
+need to re-use the string again.
+*/
+fn split_diff(diff: String) -> Result<Vec<PatchFile>, &'static str>  {
+    let mut patch_files = Vec::new();
+    let mut current_file_lines = Vec::new();
+    let mut old_file_name = String::new();
+    let mut new_file_name = String::new();
+
+    // TODO: We will need to deal with outputting CRLF correctly, in the future.
+    // Although to be fair, I'm not sure that actually matters for most patch
+    // tools. Can use diff.split_inclusive('\n').
+
+    // TOOD: Need to account for git putting a/ and b/ in front of file names.
+    for line in diff.lines() {
+        /*
+        In many patch formats, such as git, this is the indicator that
+        we are starting a new file.
+
+        We check if we have ever set old_file_name here, because some diff
+        formats have a header at the top before any file info, and we want
+        to preserve that as part of the first file. (In the future, maybe
+        we preserve it as a separate return value.)
+        */
+        if line.starts_with("diff ") && !old_file_name.is_empty() {
+            patch_files.push(PatchFile {
+                old: old_file_name.clone(),
+                new: new_file_name.clone(),
+                contents: current_file_lines.join("\n"),
+
+            });
+            current_file_lines.clear();
+            old_file_name.clear();
+            new_file_name.clear();
+        } else if line.starts_with("--- ") {
+            old_file_name = line[4..].to_owned();
+        } else if line.starts_with("+++ ") {
+            new_file_name = line[4..].to_owned();
+        }
+
+        current_file_lines.push(line);
+    }
+
+    if old_file_name.is_empty() || new_file_name.is_empty() {
+        return Err("Did not find any lines starting with --- or +++ in the diff");
+    }
+
+    patch_files.push(PatchFile {
+        old: old_file_name.clone(),
+        new: new_file_name.clone(),
+        contents: current_file_lines.join("\n"),
+
+    });
+
+    Ok(patch_files)
+
 }
